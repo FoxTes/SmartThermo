38adfb97509a3d1915a2fa9d36b50d4b4fe3f715
Georgy Levchenko <55885862+FoxTes@users.noreply.github.com>
2021-02-25 23:50:14 +0400

    Gui.

---
+++ src/SmartThermo/Models/TimeEther.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartThermo.Models
+{
+    public class TimeEther
+    {
+        public int Id { get; set; }
+        public int Time { get; set; }
+    }
+}
--- src/SmartThermo/ViewModels/MainWindowViewModel.cs
+++ src/SmartThermo/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using Prism.Mvvm;
-using SmartThermo.Enums;
-using System.Collections.ObjectModel;
-using System.IO.Ports;
-using System.Threading;
-using Prism.Commands;
-using Prism.Events;
-using SmartThermo.Service;
+﻿using Prism.Mvvm;
 using SmartThermo.Models;
+using System;
+using System.Collections.ObjectModel;
 
 namespace SmartThermo.ViewModels
 {
@@ -15,186 +9,30 @@ namespace SmartThermo.ViewModels
     {
         #region Field
 
-        private readonly ISerialPortService _serialPortService;
-
-        private bool _statusConnect;
-
-        private ObservableCollection<string> _ports = new ObservableCollection<string>();
-        private string _portSelected;
-        private string _loggerText;
-
-        private BaudRate _baudRateSelected;
-        private Parity _paritySelected;
-        private DataBits _dataBitsSelected;
-        private StopBits _stopBitsSelected;
+        private ObservableCollection<TimeEther> _sensorsEtherItems;
 
         #endregion
 
         #region Property
 
-        public bool StatusConnect
+        public ObservableCollection<TimeEther> SensorsEtherItems
         {
-            get => _statusConnect;
-            set => SetProperty(ref _statusConnect, value);
+            get => _sensorsEtherItems;
+            set => SetProperty(ref _sensorsEtherItems, value);
         }
-
-        public ObservableCollection<string> Ports
-        {
-            get => _ports;
-            set => SetProperty(ref _ports, value);
-        }
-
-        public string PortSelected
-        {
-            get => _portSelected;
-            set => SetProperty(ref _portSelected, value);
-        }
-
-        public string LoggerText
-        {
-            get => _loggerText;
-            set => SetProperty(ref _loggerText, value);
-        }
-
-        public BaudRate BaudRateSelected
-        {
-            get => _baudRateSelected;
-            set => SetProperty(ref _baudRateSelected, value);
-        }
-
-        public Parity ParitySelected
-        {
-            get => _paritySelected;
-            set => SetProperty(ref _paritySelected, value);
-        }
-
-        public DataBits DataBitsSelected
-        {
-            get => _dataBitsSelected;
-            set => SetProperty(ref _dataBitsSelected, value);
-        }
-
-        public StopBits StopBitsSelected
-        {
-            get => _stopBitsSelected;
-            set => SetProperty(ref _stopBitsSelected, value);
-        }
-
-        public DelegateCommand GetSerialPortCommand { get; }
-
-        public DelegateCommand ChangedStatusConnectCommand { get; }
-
-        public DelegateCommand WriteDataCommand { get; }
-
-        public DelegateCommand ReadDataCommand { get; }
 
         #endregion
 
-        public MainWindowViewModel(ISerialPortService serialPortService)
+        public MainWindowViewModel()
         {
-            _serialPortService = serialPortService;
-            _serialPortService.StatusConnectChanged += StatusConnectChanged;
+            var random = new Random();
 
-            Ports.AddRange(_serialPortService.GetPortNames());
-
-            PortSelected = Ports[0];
-            BaudRateSelected = BaudRate.S9600;
-            ParitySelected = Parity.None;
-            DataBitsSelected = DataBits.B8;
-            StopBitsSelected = StopBits.One;
-
-            GetSerialPortCommand = new DelegateCommand(GetSerialPortSumbit);
-            ChangedStatusConnectCommand = new DelegateCommand(ChangeStatusConnectSumbit);
-            WriteDataCommand = new DelegateCommand(WriteDataSumbit);
-            ReadDataCommand = new DelegateCommand(ReadDataSumbit);
-        }
-
-        private void WriteDataSumbit()
-        {
-            try
-            {
-                _serialPortService.WriteData(1);
-            }
-            catch (Exception ex)
-            {
-                LoggerText += ex.Message + "\n";
-            }
-        }
-
-        private  void ReadDataSumbit()
-        {
-            try
-            {
-                var test = _serialPortService.ReadData(1);
-
-                LoggerText += test[0].ToString();
-            }
-            catch (Exception ex)
-            {
-                LoggerText += ex.Message + "\n";
-            }
+            SensorsEtherItems = new ObservableCollection<TimeEther>();
+            for (var i = 0; i < 42; i++)
+                SensorsEtherItems.Add(new TimeEther { Id = i + 1, Time = random.Next(0, 60) });
         }
 
         #region Method
-
-        private void StatusConnectChanged(object sender, StatusConnect e)
-        {
-            if (e == Enums.StatusConnect.Open)
-            {
-                StatusConnect = true;
-            }
-            else
-            {
-                StatusConnect = false;
-            }
-        }
-
-        private void GetSerialPortSumbit()
-        {
-            Ports.Clear();
-            Ports.AddRange(_serialPortService.GetPortNames());
-
-            PortSelected = Ports[0];
-        }
-
-        private void ChangeStatusConnectSumbit()
-        {
-            if (_statusConnect)
-            {
-                try
-                {
-                    _serialPortService.Close();
-
-                    LoggerText += $"Порт {_serialPortService.PortName} успешно закрыт." + "\n";
-
-                }
-                catch (Exception ex)
-                {
-                    LoggerText += $"Ошибка при закрытии {_portSelected} порта." + "\n";
-                    LoggerText += ex.Message + "\n";
-                }
-            }
-            else
-            {
-                try
-                {
-                    _serialPortService.Open(new SerialPortParam()
-                    {
-                        Name = PortSelected,
-                        BaudRate = BaudRateSelected,
-                        DataBits = DataBitsSelected,
-                        Parity = ParitySelected,
-                        StopBits = StopBitsSelected
-                    });
-                    LoggerText += $"Порт {_serialPortService.PortName} успешно открыт." + "\n";
-                }
-                catch (Exception ex)
-                {
-                    LoggerText += $"Ошибка открытия порта {_serialPortService.PortName}." + "\n";
-                    LoggerText += ex.Message + "\n";
-                }
-            }
-        }
 
 
         #endregion

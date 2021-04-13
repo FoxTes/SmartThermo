8a0b4d09de154a873fb7ed499d228c45f186f8c9
Georgy Levchenko <lewshadow@gmail.com>
2021-04-13 16:43:29 +0400

    Update library and unit tests.

---
+++ tests/SmartThermo.Modules.ModuleName.Tests/Extensions.cs
@@ -0,0 +1,28 @@
+using SmartThermo.Core.Extensions;
+using Xunit;
+
+namespace SmartThermo.Modules.ModuleName.Tests
+{
+    public class Extensions
+    {
+        [Fact]
+        public void CrcCalcTest()
+        {
+            byte[] data = new byte[2] { 0x01, 0x01 };
+            var result = data.CrcCalc();
+            Assert.Equal(0x02, result);
+
+            byte[] data1 = new byte[2] { 0x00, 0x00 };
+            var result1 = data1.CrcCalc();
+            Assert.Equal(0x00, result1);
+
+            byte[] data2 = new byte[2] { 0x01, 0xFF };
+            var result2 = data2.CrcCalc();
+            Assert.Equal(0x00, result2);
+
+            byte[] data3 = new byte[2] { 0x02, 0xFF };
+            var result3 = data3.CrcCalc();
+            Assert.Equal(0x01, result3);
+        }
+    }
+}

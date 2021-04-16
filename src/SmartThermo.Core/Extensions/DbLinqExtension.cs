a6b8634dc05aaa3cfb2f61bb3b82587bbc4cc686
Georgy Levchenko <lewshadow@gmail.com>
2021-04-16 10:38:48 +0400

    Implemented the ability to select the reading of the latest records.

---
+++ src/SmartThermo.Core/Extensions/DbLinqExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SmartThermo.Core.Extensions
+{
+    public static class DbLinqExtension
+    {
+        public static IEnumerable<TSource> TakeLastEx<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector, int N)
+        {
+            return source.OrderByDescending(selector)
+                .Take(N)
+                .OrderBy(selector);
+        }
+    }
+}
